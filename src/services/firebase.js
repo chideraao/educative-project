eee48867ddf8efcee7d28a425acc5c3818011ee3
Chidera Okeke <okekechidera97@gmail.com>
2021-12-30 02:15:31 +0100

    initial commit, intro

---
+++ src/services/firebase.js
@@ -0,0 +1,30 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+var {
+  REACT_APP_apiKey,
+  REACT_APP_authDomain,
+  REACT_APP_projectId,
+  REACT_APP_storageBucket,
+  REACT_APP_messagingSenderId,
+  REACT_APP_appId,
+  REACT_APP_measurementId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey: REACT_APP_apiKey,
+  authDomain: REACT_APP_authDomain,
+  projectId: REACT_APP_projectId,
+  storageBucket: REACT_APP_storageBucket,
+  messagingSenderId: REACT_APP_messagingSenderId,
+  appId: REACT_APP_appId,
+  measurementId: REACT_APP_measurementId,
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
